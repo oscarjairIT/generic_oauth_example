58df6c26398ac6c1dc5651c5eb2ca17428086e4b
oscarjairIT <oacevedo@dhemax.cl>
2020-12-13 00:23:55 -0300

    Add Routes with namespace

---
+++ app/controllers/api/v1/flows_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::FlowsController < ApplicationController
+    respond_to :json
+
+    # Returns access credentials from .env
+    def index
+        
+    end
+
+    # Redirect to :platform OAuth
+    def flow_to
+
+    end
+end
--- app/controllers/flows_controller.rb
+++
@@ -1,2 +0,0 @@
-class FlowsController < ApplicationController
-end
