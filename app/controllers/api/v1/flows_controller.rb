c54dc9e2fa2d7bd6a00f30d44173eec2be496a0b
oscarjairIT <oacevedo@dhemax.cl>
2020-12-13 22:00:30 -0300

    Add get token from Github raw method

--- app/controllers/api/v1/flows_controller.rb
+++ app/controllers/api/v1/flows_controller.rb
@@ -12,7 +12,28 @@ class Api::V1::FlowsController < ApiController
         render json: @flows, status: :bad_request
     end
 
+    # Recieves code and Request Token
+    def get_token
+        render json: @flows = token_request(params[:code]) and return if params[:code]
+        render json: @flows, status: :bad_request
+    end
+
     private 
+
+    def token_request(code)
+        result = HTTParty.get("https://github.com/login/oauth/access_token",
+            :body => {
+                :client_id => "de397562547f887d471f",
+                :client_secret => "31001ef7542705850702c2a7f2edabc49b5467c2",
+                :code => code
+            }.to_json,
+            :headers => {
+            "Content-Type" => "application/json",
+            "Accept" => "application/json"
+        }) 
+        result = result.parsed_response
+        return result
+    end
     
     def flows_params
         params.require(:flows).permit(:platform)
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/flows_to', to: 'flows#flow_to'
+      get '/flows_token', to: 'flows#get_token'
       resources :flows
     end
   end
 
-  get '/oauth/check', to: 'oauth_check#index'
+  
 
 end
